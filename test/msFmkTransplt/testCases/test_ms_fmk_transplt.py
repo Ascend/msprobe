e6ec8df5b82bb7cbed6d5588c8f8e7df090cadaf
Develeper <Develeper>
2022-01-06 17:09:07 +0800

    Match-id-e416628264c8f57ac9894561a520dffbdbc26c7d

--- test/msFmkTransplt/testCases/test_ms_fmk_transplt.py
+++ test/msFmkTransplt/testCases/test_ms_fmk_transplt.py
@@ -15,6 +15,7 @@ from multiprocessing import Manager, Process
 
 import xmlrunner
 from xmlrunner.extra.xunit_plugin import transform
+from test_rules import TestRules as TestBuildRules
 
 import coverage
 
