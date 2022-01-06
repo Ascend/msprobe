4ce48f1d54625139006947d1f0220b59af96f7dc
Develeper <Develeper>
2022-01-06 19:27:39 +0800

    Match-id-389f5b3d095f770344e28b403e67953dfea074fb

--- test/msFmkTransplt/testCases/test_ms_fmk_transplt.py
+++ test/msFmkTransplt/testCases/test_ms_fmk_transplt.py
@@ -102,6 +102,7 @@ class TestMsFmkTransplt(unittest.TestCase):
     def result_check(self, standard_file, output_file):
         with open(standard_file, 'r', encoding='utf-8') as st_file:
             standard_content = st_file.read().splitlines()
+
         with open(output_file, 'r', encoding='utf-8') as out_file:
             output_content = out_file.read().splitlines()
         result = list(difflib.unified_diff(standard_content, output_content, n=0))
