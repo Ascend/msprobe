c7e0a94778c1fee2fb92ed7a16dafd7b8e0477e2
Develeper <Develeper>
2022-01-28 16:21:06 +0800

    Match-id-f89554dc3c7929944099b293e8f96a6eeaac5db7

--- src/ms_fmk_transplt/trans_utils.py
+++ src/ms_fmk_transplt/trans_utils.py
@@ -171,10 +171,10 @@ def walk_input_path(path, output_path):
                 continue
             if file.endswith('.py'):
                 py_file_counts += 1
-                if py_file_counts >= MAX_PYTHON_FILE_COUNT:
-                    user_interactive_confirm(
-                        f'The input path contains more than {MAX_PYTHON_FILE_COUNT} python files, '
-                        f'do you want to continue?')
+            if py_file_counts >= MAX_PYTHON_FILE_COUNT:
+                user_interactive_confirm(
+                    f'The input path contains more than {MAX_PYTHON_FILE_COUNT} python files, '
+                    f'do you want to continue?')
             total_size += os.path.getsize(file_path)
             if total_size >= output_free_size:
                 raise InputCheckException(
