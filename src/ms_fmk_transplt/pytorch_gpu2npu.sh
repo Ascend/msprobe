1a0604cac7344300b559d37beb5bbf0f1ea082f0
Develeper <Develeper>
2022-02-25 14:58:54 +0800

    Match-id-164e1fd060510d0fcd70e1baf7f1bdcb1eeb971b

--- src/ms_fmk_transplt/pytorch_gpu2npu.sh
+++ src/ms_fmk_transplt/pytorch_gpu2npu.sh
@@ -5,4 +5,4 @@ script_path=$(readlink -f "$0")
 route=$(dirname "$script_path")
 
 # run pytorch_gpu2npu
-PYTHONPATH="$route/..":$PYTHONPATH python3 "$route"/scripts/pytorch_gpu2npu.py "$@"
+PYTHONPATH="$route":$PYTHONPATH python3 "$route"/ms_fmk_transplt.py "$@"
