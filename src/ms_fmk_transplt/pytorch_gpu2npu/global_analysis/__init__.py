cf8d3f18dd4b22978e3b55082d3969473dfa173a
Develeper <Develeper>
2022-03-26 11:10:10 +0800

    Match-id-df280bb6eb74fbfe0f91ee5a55487e8fa8bfb23d

--- src/ms_fmk_transplt/pytorch_gpu2npu/global_analysis/__init__.py
+++ src/ms_fmk_transplt/pytorch_gpu2npu/global_analysis/__init__.py
@@ -2,4 +2,4 @@
 # -*- coding: utf-8 -*-
 # Copyright Huawei Technologies Co., Ltd. 2022-2022. All rights reserved.
 
-from pytorch_gpu2npu.global_analysis.global_visters import GlobalReferenceVisitor
+from pytorch_gpu2npu.global_analysis.global_vistors import GlobalReferenceVisitor
