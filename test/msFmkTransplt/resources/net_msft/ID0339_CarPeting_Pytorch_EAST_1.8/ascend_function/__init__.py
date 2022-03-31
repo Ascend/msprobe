8d7e81170f2a86211bda5b81f9223f653cf880e1
Develeper <Develeper>
2022-03-31 09:59:32 +0800

    Match-id-2d59edebb2d0462f5d61fa2366e8a2cb9b193c2a

--- test/msFmkTransplt/resources/net_msft/ID0339_CarPeting_Pytorch_EAST_1.8/ascend_function/__init__.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-# Copyright Huawei Technologies Co., Ltd. 2020-2021. All rights reserved.
-
-from .similar_api import SyncBatchNorm, ApexDistributedDataParallel, Conv3d, get_device_properties, \
-    set_default_tensor_type, repeat_interleave, TorchDistributedDataParallel, pad
-
-__all__ = ["SyncBatchNorm", "ApexDistributedDataParallel", "Conv3d", "get_device_properties",
-           "set_default_tensor_type", "repeat_interleave", "TorchDistributedDataParallel", "pad"]
