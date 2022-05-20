d95262194533559c164ed33e830a853c73e5d461
Develeper <Develeper>
2022-05-20 11:20:29 +0800

    Match-id-8667ead319586c0f11518976ea6cc4f7b4392c3c

---
+++ test/run_st.py
@@ -0,0 +1,36 @@
+import os
+import subprocess
+
+
+def run_st():
+    st_path = os.path.abspath("st/testcase/")
+    src_path = os.path.abspath("compare/")
+    src_name = "compare"
+    cur_dir = os.path.abspath('.')
+
+    cmd = ['python3.7', '-m', 'pytest', st_path,
+           '--cov=' + src_name, '--cov-report=xml:' + cur_dir + '/st_report.xml']
+    print(cmd)
+
+    result_st = subprocess.Popen(cmd, shell=False,
+                                 stdout=subprocess.PIPE,
+                                 stderr=subprocess.STDOUT)
+
+    while result_st.poll() is None:
+        line = result_st.stdout.readline()
+        line = line.strip()
+        if line:
+            print(line)
+
+    st_flag = False
+    if result_st.returncode == 0:
+        st_flag = True
+        print("run st success")
+    else:
+        print("run st failed")
+
+    return st_flag
+
+
+if __name__ == "__main__":
+    run_st()
---
+++ test/run_test.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+# This script is used to run ut and st testcase.
+# Copyright Huawei Technologies Co., Ltd. 2021-2022. All rights reserved.
+TOP_DIR=$(dirname $(pwd))
+TEST_DIR=${TOP_DIR}/"test"
+SRC_DIR=${TOP_DIR}/"src"
+
+clean() {
+  cd ${TEST_DIR}
+  if [ -e st_report ]; then
+    rm -r st_report
+    echo "remove last st_report success"
+  fi
+
+  if [ -e ut_report ]; then
+    rm -r ut_report
+    echo "remove last ut_report success"
+  fi
+
+  if [ -e compare ]; then
+    rm -r compare
+    echo "remove compare success"
+  fi
+}
+
+copy_src() {
+  local src_pkg="compare.tar.gz"
+  cd ${SRC_DIR}
+  tar -zcf src_pkg compare
+  mv ${SRC_DIR}/src_pkg ${TEST_DIR}
+  cd ${TEST_DIR}
+  tar -zxf src_pkg && rm src_pkg
+}
+
+run_st() {
+  export PYTHONPATH=PYTHONPATH:${TEST_DIR}/compare && python3 run_st.py
+}
+
+run_ut() {
+  export PYTHONPATH=PYTHONPATH:${TEST_DIR}/compare && python3 run_ut.py
+}
+
+main() {
+  clean
+  copy_src
+
+  if [[ $1 == "ut" ]] || [[ $1 == "st" ]]; then
+    if [ $1 == "ut" ]; then
+      echo "start run ut"
+      run_ut
+    elif [ $1 == "st" ]; then
+      echo "start run st"
+      run_st
+    fi
+  else
+    echo "start run ut and st"
+    run_ut
+    run_st
+  fi
+}
+
+main $@
