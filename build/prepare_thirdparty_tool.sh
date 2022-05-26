2d714a52760c8f727606c5fe2acb231a564868e8
Develeper <Develeper>
2022-05-26 16:40:47 +0800

    Match-id-f3dcff89bd13e8c072094fdf427c188168ce8bd9

--- build/prepare_thirdparty_tool.sh
+++ build/prepare_thirdparty_tool.sh
@@ -5,18 +5,10 @@ TOP_DIR=${CUR_DIR}/..
 OPENSOURCE_DIR=${TOP_DIR}/"opensource"
 PROTOBUF_DIR=${OPENSOURCE_DIR}/"protobuf"
 
-# download from opensource center
-download_thirdparty_code() {
-  cd ${OPENSOURCE_DIR}
-  if [ ! -e protobuf ]; then
-    git clone ssh://git@codehub-dg-y.huawei.com:2222/OpenSourceCenter/protobuf.git protobuf -b v3.13.0
-  fi
-}
-
 make_protobuf() {
   cd ${OPENSOURCE_DIR}
   if [ ! -e protobuf ]; then
-    download_thirdparty_code
+    echo "Can't find "${PROTOBUF_DIR}
   fi
   cmake . -Dprotobuf_BUILD_TESTS=OFF -DCMAKE_SKIP_RPATH=TRUE && make -j16
 }
