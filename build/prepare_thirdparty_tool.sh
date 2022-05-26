b40ee4b269c9f9bb42f114e26ee202a6700e2c7f
Develeper <Develeper>
2022-05-26 18:43:30 +0800

    Match-id-89efcf3c369c51d5e43b6370b7f58b8e4392a49e

--- build/prepare_thirdparty_tool.sh
+++ build/prepare_thirdparty_tool.sh
@@ -7,7 +7,7 @@ PROTOBUF_DIR=${OPENSOURCE_DIR}/"protobuf"
 
 make_protobuf() {
   cd ${OPENSOURCE_DIR}
-  if [ ! -e protobuf ]; then
+  if [ -e protobuf ]; then
     cmake . -Dprotobuf_BUILD_TESTS=OFF -DCMAKE_SKIP_RPATH=TRUE && make -j16
   else
     echo "Can't find "${PROTOBUF_DIR}
