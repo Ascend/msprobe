29518dee75549e624ce600cabcf0cee81f5b6ff4
Develeper <Develeper>
2022-06-06 20:31:43 +0800

    Match-id-07e53b2f89a229bd02477b4df21bbb7fe8724590

--- src/compare/advisor/model.py
+++ src/compare/advisor/model.py
@@ -8,6 +8,10 @@ import json
 import os
 from advisor.mscmp_advisor import CompareAdvisor
 
+CLASS_TYPE = {'op': '0', 'model': '1'}
+ERROR_CODE = {'success': '0', 'optimized': '1'}
+EXTEND_TYPE = {'list': '0', 'table': '1', 'sourcedata': '2'}
+EXTEND_DATA_TYPE = {'str': '0', 'int': '1', 'double': '2'}
 
 class ExtendResult:
     def __init__(self):
@@ -20,8 +24,8 @@ class ExtendResult:
 
 class Result:
     def __init__(self):
-        self.class_type = '0'
-        self.error_code = '0'
+        self.CLASS_TYPE = '0'
+        self.ERROR_CODE = '0'
         self.summary = ""
         self.extend_result = []
 
@@ -31,19 +35,13 @@ class Result:
             data = {"type": item.type, "extendTitle": item.extend_title,
                     "dataType": item.data_type, "key": item.key, "value": item.value}
             extend_data.append(data)
-        res = {"classType": self.class_type, "errorCode": self.error_code,
+        res = {"classType": self.CLASS_TYPE, "errorCode": self.ERROR_CODE,
                "summary": self.summary, "extendResult": extend_data}
         outputstr = json.dumps(res)
         return outputstr
 
 
-class_type = {'op': '0', 'model': '1'}
-error_code = {'success': '0', 'optimized': '1'}
-extend_type = {'list': '0', 'table': '1', 'sourcedata': '2'}
-extend_data_type = {'str': '0', 'int': '1', 'double': '2'}
-
-
-def evaluate(data_path, input_nodes=[]):
+def Evaluate(data_path, input_nodes=[]):
     """
     interface function called by msadvisor
     Args:
@@ -53,6 +51,7 @@ def evaluate(data_path, input_nodes=[]):
         json string of result info
         result must by ad_result
     """
+    # do evaluate work by file data
     # my code begin
     input_file = os.path.realpath(data_path)
 
@@ -63,54 +62,51 @@ def evaluate(data_path, input_nodes=[]):
                    "Operator Index": advisor_result.operator_index,
                    "Expert Advice": advisor_result.advisor_message}
 
-    # do evaluate work by file data
-
     # fill result
     result = Result()
-    result.class_type = class_type['model']
-    result.error_code = error_code['success']
-    result.summary = "Op operations need to be optimized"
+    result.CLASS_TYPE = CLASS_TYPE.get('model')
+    result.ERROR_CODE = ERROR_CODE.get('success')
+    result.summary = "Suggestions for accuracy comparison of the entire network"
     extend_result = ExtendResult()
-    extend_result.type = extend_type['list']
-    # extendResult.type = extend_type['table']
+    # the value of extend_result.type cat be 'list' 'table' 'sourcedata'
+    extend_result.type = EXTEND_TYPE.get('table')
 
     # list type result
     if extend_result.type == '0':
         extend_result.extend_title = "Recommendations of Ops_Not_Support_Heavy_Format"
-        extend_result.data_type.append(extend_data_type['str'])
+        extend_result.data_type.append(EXTEND_DATA_TYPE.get('str'))
         extend_result.value.append("Modify the operation to support light format")
         extend_result.value.append("Modify the operation to support heavy format")
         result.extend_result.append(extend_result)
 
     # table type result
     elif extend_result.type == '1':
-        extend_result.extend_title = "Op list"
-        extend_result.key.append("Op Name")
-        extend_result.key.append("Aicore Time(us)")
-        extend_result.key.append("Bottleneck Pathway")
+        extend_result.extend_title = "suggestions"
+        extend_result.key.append("Detection Type")
+        extend_result.key.append("Operator Index")
+        extend_result.key.append("Expert Advice")
 
-        extend_result.data_type.append(extend_data_type['str'])
-        extend_result.data_type.append(extend_data_type['double'])
-        extend_result.data_type.append(extend_data_type['str'])
+        extend_result.data_type.append(EXTEND_DATA_TYPE.get('str'))
+        extend_result.data_type.append(EXTEND_DATA_TYPE.get('str'))
+        extend_result.data_type.append(EXTEND_DATA_TYPE.get('str'))
 
         value = []
-        value.append("format trans")
-        value.append("100.52")
-        value.append("mte2->ddr")
+        value.append(result_dict.get("Detection Type"))
+        value.append(result_dict.get("Operator Index"))
+        value.append(result_dict.get("Expert Advice"))
         extend_result.value.append(value)
 
-        value1 = []
-        value1.append("add")
-        value1.append("152.55")
-        value1.append("mte3->ub")
-        extend_result.value.append(value1)
-
+        result.extend_result.append(extend_result)
+    elif extend_result.type == '2':
+        extend_result.extend_title = "sourcedatapath"
+        extend_result.data_type.append(EXTEND_DATA_TYPE.get('str'))
+        extend_result.value.append("/home/datapath/")
         result.extend_result.append(extend_result)
     return result.generate()
 
 
 if __name__ == "__main__":
-    my_data_path = "/home/wangchao/compare_tools/advisor/problem.csv"
+    DATA_PATH = "./"
     my_input_nodes = []
-    ret = evaluate(my_data_path, my_input_nodes)
-    print("sample in:{} out:{}".format(my_data_path, ret))
+    ret = Evaluate(DATA_PATH, my_input_nodes)
+    # using debug: print("sample in:{} out:{}".format(DATA_PATH, ret))
