74625eabf5ae2059c06f32a6b09e3f375ba99f5c
Develeper <Develeper>
2022-11-10 15:48:27 +0800

    Match-id-febb18e51b476e8d7ddf077cf62d794d2d9027b3

---
+++ src/ms_fmk_transplt/pytorch_analyse.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#Copyright (c) Huawei Technologies Co., Ltd. 2022-2022. All rights reserved.
+# ================================================================================
+
+# get script path
+script_path=$(readlink -f "$0")
+route=$(dirname "$script_path")
+
+# run transfer
+PYTHONPATH="$route":$PYTHONPATH python3 "$route"/pytorch_analyse.py "$@"
