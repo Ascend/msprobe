f2deccc3208fc90beb5c1539fa6b23724a73f0d4
Develeper <Develeper>
2023-01-16 17:35:11 +0800

    Match-id-f7688df2b9e0e9ed0bee921a7aefdba488e6f3ef

--- src/compare/const_manager.py
+++ src/compare/const_manager.py
@@ -36,7 +36,7 @@ class ConstManager:
     OVERFLOW_MAX_VALUE = 65504
     MAGIC_KEY_WORD = 'magic'
     MAGIC_NUM = 0x5a5a5a5a
-    INVALID_ID = 66535
+    INVALID_ID = 65535
 
     # algorithm
     FLOAT_EPSILON = np.finfo(float).eps
