a2dc4189958a12c917848e19a110e99c4218da49
Develeper <Develeper>
2023-03-22 17:37:12 +0800

    Match-id-cfb43102cf1e0b7ff0b0c7b338404bb103314860

--- src/compare/dump_data_object.py
+++ src/compare/dump_data_object.py
@@ -85,7 +85,7 @@ class DumpDataObj:
         get slice_instance_num
         @return: slice number
         """
-        return self.attr.get("slice_instance_num", None)
+        return self.attr["slice_instance_num"] if self.attr else None
 
     @property
     def get_cut_axis_manual(self: any) -> list:
