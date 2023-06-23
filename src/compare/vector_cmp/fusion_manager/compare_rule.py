5bb5ebd0e52d80b011246190c20ee2b57253bae6
Develeper <Develeper>
2023-06-23 10:46:14 +0800

    Match-id-c81018c2eb36722d97c356a4dd4f15cc4d7e21b9

--- src/compare/vector_cmp/fusion_manager/compare_rule.py
+++ src/compare/vector_cmp/fusion_manager/compare_rule.py
@@ -24,17 +24,10 @@ class CompareRule:
     """
     def __init__(self: any, fusion_json_file_path: str, quant_fusion_rule_file_path: str,
                  close_fusion_rule_file_path: str = '') -> None:
-        self.fusion_json_file_path = self._get_real_path_with_default(fusion_json_file_path)
-        self.quant_fusion_rule_file_path = self._get_real_path_with_default(quant_fusion_rule_file_path)
-        self.close_fusion_rule_file_path = self._get_real_path_with_default(close_fusion_rule_file_path)
+        self.fusion_json_file_path = self.get_real_path_with_default(fusion_json_file_path)
+        self.quant_fusion_rule_file_path = self.get_real_path_with_default(quant_fusion_rule_file_path)
+        self.close_fusion_rule_file_path = self.get_real_path_with_default(close_fusion_rule_file_path)
         self.fusion_info = None
-
-    def _get_real_path_with_default(self: any, file_path: str) -> str:
-        if file_path != '':
-            return os.path.realpath(file_path)
-        return ''
-
-
 
     @staticmethod
     def _sort_file_by_timestamp(dump_info: DumpInfo) -> dict:
@@ -142,3 +135,8 @@ class CompareRule:
 
         self.fusion_info = FusionRuleParser('')
         self.fusion_info.fusion_op_name_to_op_map = op_name_to_op_map
+
+    def get_real_path_with_default(self: any, file_path: str) -> str:
+        if file_path != '':
+            return os.path.realpath(file_path)
+        return ''
