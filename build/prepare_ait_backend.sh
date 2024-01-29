806087d1059776f99a98a96117d2171bc5f912de
Develeper <Develeper>
2024-01-29 13:36:24 +0800

    Match-id-3ad9624b7b9c84d0e2246f2eaec212fe7c2c02de

--- build/prepare_ait_backend.sh
+++ build/prepare_ait_backend.sh
@@ -20,8 +20,19 @@ function fn_build_nlohmann_json()
   rm nlohmannjson-v3.11.2.tar.gz
 }
 
+function fn_clone_securec()
+{
+  cd ${CUR_DIR}/"../ait_backend/llm/dump/"
+  if [ -d "${AIT_DIR}/dump/securec" ]; then
+      return $?
+  fi
+
+  git clone https://codehub-dg-y.huawei.com/hwsecurec_group/huawei_secure_c.git securec -b tag_Huawei_Secure_C_V100R001C01SPC012B002_00001
+}
+
 make_ait_backend() {
   fn_build_nlohmann_json
+  fn_clone_securec
   cd ${AIT_DIR} && echo "Start building project \"${PROJECT_NAME}\""
   if [ ! -d "${BUILD_TYPE}" ]; then
       mkdir "${BUILD_TYPE}"
