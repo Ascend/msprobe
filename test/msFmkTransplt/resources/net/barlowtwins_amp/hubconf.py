81f0042c5b5589c4a0e03c44749b5bc436616818
Develeper <Develeper>
2025-12-17 10:32:09 +0800

    Match-id-a79832a49a8f7403b168ac8b749b5453776373d5

--- test/msFmkTransplt/resources/net/barlowtwins_amp/hubconf.py
+++ test/msFmkTransplt/resources/net/barlowtwins_amp/hubconf.py
@@ -1,8 +1,18 @@
-# Copyright (c) Facebook, Inc. and its affiliates.
-# All rights reserved.
+# -------------------------------------------------------------------------
+#  This file is part of the MindStudio project.
+# Copyright (c) 2025 Huawei Technologies Co.,Ltd.
 #
-# This source code is licensed under the license found in the
-# LICENSE file in the root directory of this source tree.
+# MindStudio is licensed under Mulan PSL v2.
+# You can use this software according to the terms and conditions of the Mulan PSL v2.
+# You may obtain a copy of Mulan PSL v2 at:
+#
+#          http://license.coscl.org.cn/MulanPSL2
+#
+# THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+# EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+# MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+# See the Mulan PSL v2 for more details.
+# -------------------------------------------------------------------------
 
 import torch
 from torchvision.models.resnet import resnet50 as _resnet50
