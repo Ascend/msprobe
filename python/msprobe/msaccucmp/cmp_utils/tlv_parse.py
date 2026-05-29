8299d775ff2ac32933093c71f55ce844661c3c59
ascend-robot <zhongyuanke@huawei.com>
2026-05-29 14:19:05 +0800

    【bugfix】tlv_parse中tlv_list_len校验修改
    Co-authored-by: l30036321<lvkaimeng@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !701 merge master into master
    
    【bugfix】tlv_parse中tlv_list_len校验修改
    
    Created-by: lv-kaimeng
    Commit-by: l30036321;lv-kaimeng
    Merged-by: ascend-robot
    Description: 修改tlv_parse校验
    
    See merge request: Ascend/msprobe!701

--- python/msprobe/msaccucmp/cmp_utils/tlv_parse.py
+++ python/msprobe/msaccucmp/cmp_utils/tlv_parse.py
@@ -50,6 +50,7 @@ tlv = TLV(TLV_CONFIG_TAB)
 aux, input = tlv.parse_tlv_by_cfg_tab(aux, input)
 
 """
+
 import struct
 
 from cmp_utils import log
@@ -64,16 +65,16 @@ INT64_SIZE = 8
 UINT64_SIZE = 8
 
 _UNPACK_FORMAT = {
-    'CHAR': {'FMT': 's', 'SIZE': 1},
-    'UINT16': {'FMT': 'H', 'SIZE': 2},
-    'INT16': {'FMT': 'h', 'SIZE': 2},
-    'UINT32': {'FMT': 'I', 'SIZE': 4},
-    'INT32': {'FMT': 'i', 'SIZE': 4},
-    'UINT64': {'FMT': 'Q', 'SIZE': 8},
-    'INT64': {'FMT': 'q', 'SIZE': 8},
+    "CHAR": {"FMT": "s", "SIZE": 1},
+    "UINT16": {"FMT": "H", "SIZE": 2},
+    "INT16": {"FMT": "h", "SIZE": 2},
+    "UINT32": {"FMT": "I", "SIZE": 4},
+    "INT32": {"FMT": "i", "SIZE": 4},
+    "UINT64": {"FMT": "Q", "SIZE": 8},
+    "INT64": {"FMT": "q", "SIZE": 8},
 }
 
-TLV_TYPE = ['ATOM', 'TLV', 'TLNV', 'NV']
+TLV_TYPE = ["ATOM", "TLV", "TLNV", "NV"]
 
 
 class NestedObj:
@@ -89,7 +90,7 @@ class TLV:
     @staticmethod
     def _unpack_single_element(aux: bytes, ele_type) -> (bytes, any):
         type_para = _UNPACK_FORMAT.get(ele_type)
-        ele_fmt, ele_size = type_para.get('FMT'), type_para.get('SIZE')
+        ele_fmt, ele_size = type_para.get("FMT"), type_para.get("SIZE")
 
         if len(aux) < ele_size:
             raise CompareError(CompareError.MSACCUCMP_PARSE_NANO_DUMP_FILE_ERROR)
@@ -98,68 +99,77 @@ class TLV:
         except struct.error as error:
             raise CompareError(CompareError.MSACCUCMP_PARSE_NANO_DUMP_FILE_ERROR) from error
 
-        if ele_type == 'CHAR' and isinstance(value, bytes):
+        if ele_type == "CHAR" and isinstance(value, bytes):
             value = value.decode(encoding="utf-8")
 
         return aux[ele_size:], value
 
     @staticmethod
     def _check_tl(tag, length, tlv_cfg):
-        tag_in_cfg, ele_type = tlv_cfg.get("Tag"), tlv_cfg.get('Ele_Type')
+        tag_in_cfg, ele_type = tlv_cfg.get("Tag"), tlv_cfg.get("Ele_Type")
 
         if tag_in_cfg != tag:
-            log.print_error_log(
-                'Failed to parse of tag_in_cfg: %s, tag_in_stream: %s.' % (tag_in_cfg, tag))
+            log.print_error_log("Failed to parse of tag_in_cfg: %s, tag_in_stream: %s." % (tag_in_cfg, tag))
             raise CompareError(CompareError.MSACCUCMP_PARSE_NANO_DUMP_FILE_ERROR)
 
         if not isinstance(ele_type, list):
-            ele_size = _UNPACK_FORMAT.get(ele_type).get('SIZE')
-            if length < ele_size and length!=0:
+            ele_size = _UNPACK_FORMAT.get(ele_type).get("SIZE")
+            if length < ele_size and length != 0:
                 log.print_error_log(
-                    'Failed to parse of tag: %s. length:%d < single ele_size:%d' % (tag_in_cfg, length, ele_size))
+                    "Failed to parse of tag: %s. length:%d < single ele_size:%d" % (tag_in_cfg, length, ele_size)
+                )
                 raise CompareError(CompareError.MSACCUCMP_PARSE_NANO_DUMP_FILE_ERROR)
 
     def parse_tlv_by_cfg_tab(self, content: bytes, x: object) -> (bytes, object):
         aux = content
         self._check_cfg_tab(self.tlv_tab)
 
+        tlv_remaining = -1
+
         for tlv_cfg in self.tlv_tab:
-            tlv_type = tlv_cfg.get('TLV_Type')
-            if tlv_type == 'ATOM':
+            tlv_type = tlv_cfg.get("TLV_Type")
+            if tlv_type == "ATOM":
                 aux, x = self._unpack_atom_value(aux, tlv_cfg, x)
-            elif tlv_type == 'TLV':
+                if tlv_cfg.get("Name") == "tlv_list_len":
+                    tlv_remaining = getattr(x, "tlv_list_len", -1)
+            elif tlv_type == "TLV":
                 aux, x = self._unpack_tlv_value(aux, tlv_cfg, x)
-            elif tlv_type == 'TLNV':
-                aux, x = self._unpack_tlnv_value(aux, tlv_cfg, x)
-            elif tlv_type == 'NV':
-                num_attr_name = tlv_cfg.get('N')
+            elif tlv_type == "TLNV":
+                if tlv_remaining != -1 and tlv_remaining <= 0:
+                    setattr(x, tlv_cfg.get("Name"), None)
+                else:
+                    aux_before = len(aux)
+                    aux, x = self._unpack_tlnv_value(aux, tlv_cfg, x)
+                    if tlv_remaining >= 0:
+                        tlv_remaining -= aux_before - len(aux)
+            elif tlv_type == "NV":
+                num_attr_name = tlv_cfg.get("N")
                 num = getattr(x, num_attr_name)
                 aux, x = self._unpack_nv_value(aux, tlv_cfg, x, num)
         return aux, x
 
     def _check_cfg_tab(self, tlv_tab):
         for tlv_cfg in tlv_tab:
-            tlv_type, ele_type = tlv_cfg.get('TLV_Type'), tlv_cfg.get('Ele_Type')
+            tlv_type, ele_type = tlv_cfg.get("TLV_Type"), tlv_cfg.get("Ele_Type")
             if tlv_type not in TLV_TYPE:
-                log.print_error_log('%s invalid tlv_type: %s.' % (tlv_cfg.get('Name'), tlv_type))
+                log.print_error_log("%s invalid tlv_type: %s." % (tlv_cfg.get("Name"), tlv_type))
                 raise CompareError(CompareError.MSACCUCMP_PARSE_NANO_DUMP_FILE_ERROR)
 
             # Nested tlv
             if isinstance(ele_type, list):
                 self._check_cfg_tab(ele_type)
             elif _UNPACK_FORMAT.get(ele_type) is None:
-                log.print_error_log('invalid ele_type: %s.' % ele_type)
+                log.print_error_log("invalid ele_type: %s." % ele_type)
                 raise CompareError(CompareError.MSACCUCMP_PARSE_NANO_DUMP_FILE_ERROR)
 
-            if tlv_type == 'NV':
-                num_attr_name = tlv_cfg.get('N')
+            if tlv_type == "NV":
+                num_attr_name = tlv_cfg.get("N")
                 if num_attr_name is None:
                     log.print_error_log("is tlv_tpye is NV, 'N' must be given!")
                     raise CompareError(CompareError.MSACCUCMP_PARSE_NANO_DUMP_FILE_ERROR)
-        return
 
     def _unpack_atom_value(self, aux: bytes, tlv_cfg, x: object) -> (bytes, object):
-        ele_type, ele_name = tlv_cfg.get('Ele_Type'), tlv_cfg.get('Name')
+        ele_type, ele_name = tlv_cfg.get("Ele_Type"), tlv_cfg.get("Name")
 
         if isinstance(ele_type, list):
             # parse Nested tlv value
@@ -172,8 +182,10 @@ class TLV:
                 # parse single element
                 aux, value = self._unpack_single_element(aux, ele_type)
             except CompareError as error:
-                log.print_error_log('Failed to parse of ele name: %s, value_type: %s, '
-                                    'Please check the dump file:%s.' % (tlv_cfg.get('Name'), ele_type, str(error)))
+                log.print_error_log(
+                    "Failed to parse of ele name: %s, value_type: %s, "
+                    "Please check the dump file:%s." % (tlv_cfg.get("Name"), ele_type, str(error))
+                )
                 raise CompareError(CompareError.MSACCUCMP_PARSE_NANO_DUMP_FILE_ERROR) from error
 
         setattr(x, ele_name, value)
@@ -185,19 +197,19 @@ class TLV:
         return aux, tag, length
 
     def _unpack_tlv_value(self, aux: bytes, tlv_cfg, x: object) -> (bytes, object):
-        ele_type, ele_name = tlv_cfg.get('Ele_Type'), tlv_cfg.get('Name')
+        ele_type, ele_name = tlv_cfg.get("Ele_Type"), tlv_cfg.get("Name")
 
         # tag, len
         try:
             aux_try, tag, length = self._parse_tag_len(aux)
         except CompareError:
             setattr(x, ele_name, None)
-            log.print_info_log('Failed to parse of ele name: %s, ele_type: %s' % (ele_name, ele_type))
+            log.print_info_log("Failed to parse of ele name: %s, ele_type: %s" % (ele_name, ele_type))
             return aux, x
         try:
             self._check_tl(tag, length, tlv_cfg)
         except CompareError:
-            log.print_info_log('Failed to parse of ele name: %s, ele_type: %s' % (ele_name, ele_type))
+            log.print_info_log("Failed to parse of ele name: %s, ele_type: %s" % (ele_name, ele_type))
             setattr(x, ele_name, None)
             return aux, x
 
@@ -205,19 +217,23 @@ class TLV:
         return new_aux, new_x
 
     def _unpack_tlnv_value(self, aux: bytes, tlv_cfg, x: object) -> (bytes, object):
-        tag_in_cfg, ele_type, ele_name = tlv_cfg.get("Tag"), tlv_cfg.get('Ele_Type'), tlv_cfg.get('Name')
+        _, ele_type, ele_name = (
+            tlv_cfg.get("Tag"),
+            tlv_cfg.get("Ele_Type"),
+            tlv_cfg.get("Name"),
+        )
 
         # tag, len
         try:
             aux_try, tag, length = self._parse_tag_len(aux)
         except CompareError:
             setattr(x, ele_name, None)
-            log.print_info_log('skip to parse tlv of ele name: %s, ele_type: %s' % (ele_name, ele_type))
+            log.print_info_log("skip to parse tlv of ele name: %s, ele_type: %s" % (ele_name, ele_type))
             return aux, x
         try:
             self._check_tl(tag, length, tlv_cfg)
         except CompareError:
-            log.print_info_log('skip to parse tlv of ele name: %s, ele_type: %s' % (ele_name, ele_type))
+            log.print_info_log("skip to parse tlv of ele name: %s, ele_type: %s" % (ele_name, ele_type))
             setattr(x, ele_name, None)
             return aux, x
 
@@ -225,33 +241,35 @@ class TLV:
 
         # get list num of value
         if _UNPACK_FORMAT.get(ele_type) is not None:
-            ele_size = _UNPACK_FORMAT.get(ele_type).get('SIZE')
+            ele_size = _UNPACK_FORMAT.get(ele_type).get("SIZE")
             ele_num = int(length / ele_size)
 
             try:
                 aux, x = self._unpack_nv_value(aux, tlv_cfg, x, ele_num)
             except CompareError as error:
-                log.print_error_log('Failed to parse of ele name: %s, value_type: %s, '
-                                    'Please check the dump file:%s.' % (tlv_cfg.get('Name'), ele_type, str(error)))
+                log.print_error_log(
+                    "Failed to parse of ele name: %s, value_type: %s, "
+                    "Please check the dump file:%s." % (tlv_cfg.get("Name"), ele_type, str(error))
+                )
                 raise CompareError(CompareError.MSACCUCMP_PARSE_NANO_DUMP_FILE_ERROR) from error
 
         elif isinstance(ele_type, list):
             value = []
             sub_aux = aux[:length]
 
-            while (len(sub_aux) > 0):
+            while len(sub_aux) > 0:
                 # Nested tlv
                 sub_tlv_tab = ele_type
                 sub_tlv, nested_obj = TLV(sub_tlv_tab), NestedObj()
                 sub_aux, single_value = sub_tlv.parse_tlv_by_cfg_tab(sub_aux, nested_obj)
                 value.append(single_value)
 
-            setattr(x, tlv_cfg.get('Name'), value)
+            setattr(x, tlv_cfg.get("Name"), value)
             aux = aux[length:]
         return aux, x
 
     def _unpack_nv_value(self, aux: bytes, tlv_cfg, x: object, ele_num) -> (bytes, object):
-        ele_type, ele_name = tlv_cfg.get('Ele_Type'), tlv_cfg.get('Name')
+        ele_type, _ = tlv_cfg.get("Ele_Type"), tlv_cfg.get("Name")
         value = []
 
         for _ in range(ele_num):
@@ -266,14 +284,16 @@ class TLV:
                     # single element
                     aux, single_value = self._unpack_single_element(aux, ele_type)
                 except CompareError as error:
-                    log.print_error_log('Failed to parse of ele name: %s, value_type: %s, '
-                                        'Please check the dump file:%s.' % (tlv_cfg.get('Name'), ele_type, str(error)))
+                    log.print_error_log(
+                        "Failed to parse of ele name: %s, value_type: %s, "
+                        "Please check the dump file:%s." % (tlv_cfg.get("Name"), ele_type, str(error))
+                    )
                     raise CompareError(CompareError.MSACCUCMP_PARSE_NANO_DUMP_FILE_ERROR) from error
                 value.append(single_value)
 
-        if ele_type == 'CHAR' and isinstance(value, list):
+        if ele_type == "CHAR" and isinstance(value, list):
             value_str = "".join(value)
-            setattr(x, tlv_cfg.get('Name'), value_str)
+            setattr(x, tlv_cfg.get("Name"), value_str)
         else:
-            setattr(x, tlv_cfg.get('Name'), value)
+            setattr(x, tlv_cfg.get("Name"), value)
         return aux, x
